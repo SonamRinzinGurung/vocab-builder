b41485c35b72b0e3543d71f3751fedad2df33c84
Sonam Rinzin Gurung <sonamrinzingurung@gmail.com>
2024-04-09 17:56:23 +0545

    Moved dictionary data from node_modules to public folder for production use

--- src/utils/getWordSuggestion.jsx
+++ src/utils/getWordSuggestion.jsx
@@ -2,7 +2,7 @@ import Typo from "typo-js";
 
 const getWordSuggestion = (word, setSuggestedWords) => {
   const dictionary = new Typo("en_US", false, false, {
-    dictionaryPath: "node_modules/typo-js/dictionaries",
+    dictionaryPath: "/dictionaries",
   });
   const isMistake = !dictionary.check(word);
   if (isMistake) {
