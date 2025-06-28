f1cd7e368c533c5aa5711a81c3672afc310ccf77
Sonam Rinzin Gurung <sonamrinzingurung@gmail.com>
2025-06-28 10:13:58 +0545

    Vocab Test: move correct words to valley

--- src/routes/VocabTest.jsx
+++ src/routes/VocabTest.jsx
@@ -1,25 +1,31 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import useSetTitle from "../hooks/useSetTitle";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, doc, getDocs, query, updateDoc, where } from "firebase/firestore";
 import { db } from "../firebase-config";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { shuffleArray } from "../utils/shuffleArray";
+import { toast } from "react-toastify";
 
 const VocabTest = ({ user }) => {
     useSetTitle("Vocab Test");
     const [result, setResult] = useState(null); // stores all the search results or the fetched data
-
     const [testStep, setTestStep] = useState(0); // tracks the current step in the test
-
     const [wordCount, setWordCount] = useState(10); // tracks the number of words in the test
+    const [testWords, setTestWords] = useState([]); // stores the words selected for the test
+    const [questionOptions, setQuestionOptions] = useState([]); // stores the options for each question in the test
+    const [selectedOptions, setSelectedOptions] = useState({}); // stores the user's selected options for each question
 
-    const [testWords, setTestWords] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [userAnswers, setUserAnswers] = useState([]);
-    const [isTestComplete, setIsTestComplete] = useState(false);
-    const [questionOptions, setQuestionOptions] = useState([]);
-    const [selectedOptions, setSelectedOptions] = useState({});
+    const queryClient = useQueryClient();
+
+    // Function to reset the test states
+    const resetTest = () => {
+        setTestStep(0);
+        setWordCount(10);
+        setTestWords([]);
+        setQuestionOptions([]);
+        setSelectedOptions({});
+    }
 
     const {
         error,
@@ -45,6 +51,7 @@ const VocabTest = ({ user }) => {
         refetchOnWindowFocus: false,
     });
 
+    // ensure wordCount does not exceed the number of available words
     useEffect(() => {
         if (wordCount > result?.length) {
             setWordCount(result?.length || 10);
@@ -63,6 +70,9 @@ const VocabTest = ({ user }) => {
     }
 
     const handleStartTest = () => {
+
+        resetTest();
+
         setTestStep(1);
 
         const shuffledWords = shuffleArray(result);
@@ -72,19 +82,45 @@ const VocabTest = ({ user }) => {
         // Generate options for each word and store them
         const optionsArr = limitedWords?.map(word => generateOptions(word, result));
         setQuestionOptions(optionsArr);
-        console.log(optionsArr)
-
-        setCurrentIndex(0);
-        setUserAnswers([]);
-        setIsTestComplete(false);
-        setSelectedOptions({});
+        // console.log(optionsArr)
 
     };
 
-    // const handleAnswer = (selectedMeaning)=>{
-    //     const currentWord = testWords[currentIndex];
-    //     const correct = currentWord.meaning === selectedMeaning;
-    // }
+    // Mutation to move the word to Vocab Valley
+    const { mutate: moveWord } = useMutation({
+        mutationFn: async (id) => {
+            const docRef = doc(db, "vocab", id);
+            await updateDoc(docRef, {
+                group: "vocab-valley",
+            });
+        },
+        onSuccess: () => {
+        },
+        onError: (error) => {
+            console.log(error);
+            toast.error("Error occurred!");
+        },
+    });
+
+    const handleMoveToVocabValley = () => {
+        try {
+            const correctAnswers = testWords.filter((word, index) => selectedOptions[index] === word.meanings[0].definitions[0].definition);
+
+            console.log(correctAnswers);
+
+            correctAnswers.forEach((word) => {
+                moveWord(word.id);
+            });
+
+            queryClient.invalidateQueries("vocab-mountain");
+            toast.success(`Word moved successfully to vocab valley`);
+            resetTest();
+
+        } catch (error) {
+            console.error("Error moving words to Vocab Valley:", error);
+            toast.error("Error moving words to Vocab Valley");
+        }
+    }
 
     if (error) return "An error has occurred: " + error.message;
     if (isPending || isQueryLoading) return null;
@@ -180,7 +216,8 @@ const VocabTest = ({ user }) => {
                                 <strong>Score:</strong> {Math.round((testWords.filter((word, index) => selectedOptions[index] === word.meanings[0].definitions[0].definition).length / testWords.length) * 100)}%
                             </p>
 
-                            <button onClick={() => setTestStep(0)}>Retake Test</button>
+                            <button onClick={resetTest}>Retake Test</button>
+                            <button onClick={handleMoveToVocabValley}>Move correct answer to Vocab Valley</button>
                         </div>
                     )
                 }
